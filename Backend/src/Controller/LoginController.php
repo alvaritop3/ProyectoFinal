ff809be8e0aa8f83258ddd6dcbafb6baa07c101f
Alvaro <apasrom619@iesmartinezm.es>
2023-05-03 21:19:11 +0200

    Creada la entidad usuario con seguridad, permite el registro con password hasheada y al hacer login devuelve el token

---
+++ Backend/src/Controller/LoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Usuario;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use DateTime;
+
+class LoginController extends AbstractController
+{
+    #[Route('/registro', name: 'app_registro', methods: ["POST"])]
+    public function registrar(UserPasswordHasherInterface $passwordHasher, ManagerRegistry $doctrine, Request $request): Response
+    {
+        //Creamos el entityManager
+        $entityManager = $doctrine->getManager();
+
+        //Recogemos los datos que vienen en la Request
+        $jsonData = $request->getContent();
+        $data = json_decode($jsonData);
+        $nombre = $data->nombre;
+        $apellidos = $data->apellidos;
+        $email = $data->email;
+        $telefono = $data->telefono;
+        $direccion = $data->direccion;
+        $password = $data->password;
+        //$roles = $data->roles;
+
+        //Creamos usuario
+        $usuario = new Usuario();
+
+        //Comprobamos que la fecha no venga vacia
+        if (isset($data->fecha_incorp)) {
+            //Transformamos la fecha de nacimiento
+            $fechaString = $data->fecha_incorp;
+            $timestamp = strtotime($fechaString);
+            $fecha_incorp = new DateTime();
+            $fecha_incorp->setTimestamp($timestamp);
+
+            $usuario->setFechaIncorp($fecha_incorp);
+        }else{
+            $usuario->setFechaIncorp(null);
+        }
+
+        // Hasheamos la contraseña
+        $hashedPassword = $passwordHasher->hashPassword(
+            $usuario,
+            $password
+        );
+
+        $usuario->setNombre($nombre);
+        $usuario->setApellidos($apellidos);
+        $usuario->setEmail($email);
+        $usuario->setTelefono($telefono);
+        $usuario->setDireccion($direccion);
+        $usuario->setPassword($hashedPassword);
+        //$usuario->setRoles($roles);
+        $usuario->setRoles(['ROLE_TUTOR']);
+
+        $entityManager->persist($usuario);
+        $entityManager->flush();
+
+        
+
+        return $this->json('Usuario ' . $usuario->getNombre() . ' creado con id ' . $usuario->getId());
+    }
+}
---
+++ Backend/src/Entity/Usuario.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuarioRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UsuarioRepository::class)]
+class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $apellidos = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telefono = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $fecha_incorp = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $direccion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(string $apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(string $telefono): self
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getFechaIncorp(): ?\DateTimeInterface
+    {
+        return $this->fecha_incorp;
+    }
+
+    public function setFechaIncorp(?\DateTimeInterface $fecha_incorp): self
+    {
+        $this->fecha_incorp = $fecha_incorp;
+
+        return $this;
+    }
+
+    public function getDireccion(): ?string
+    {
+        return $this->direccion;
+    }
+
+    public function setDireccion(string $direccion): self
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+}
